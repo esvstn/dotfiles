57bc5a86bdc451c25d8a0ded98cfb036e8b477d2
Evgeniy Savostin <esvstn@gmail.com>
2018-07-20 15:35:22 +0000

    Fix update_dotfiles

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,5 +1,9 @@
 
 function inst() {
     echo "cp $1 ~/$1"
-    cp $1 ~/$1 || { echo "Can't do cp"; exit 1; }
+    if [ -d $1 ]; then
+        cp -vr $1 ~/ || { echo "Can't do cp"; exit 1; }
+    else
+        cp -v $1 ~/$1 || { echo "Can't do cp"; exit 1; }
+    fi
 }
